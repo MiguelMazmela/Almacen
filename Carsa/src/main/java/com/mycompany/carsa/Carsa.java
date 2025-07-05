91ca4cf37b58ad5cc38a30aae419f1f7c6aac699
Miguel Mázmela <miguelmazmela@gmail.com>
2025-07-04 23:45:31 -0500

    inicio de un proyecto de almacenes y otros para carsa

---
+++ Carsa/src/main/java/com/mycompany/carsa/Carsa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carsa;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Carsa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
